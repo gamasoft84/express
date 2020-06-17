d574c0ebb5b0ee353897fd0ad53d3e3144c25c95
gamasoft84 <richardgama@yahoo.com.mx>
2020-06-17 12:19:23 -0500

    Se actualiza al puerto 8080

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get('/greet', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3030, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
 module.exports = createHandler(app);
